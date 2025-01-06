eb865a6414e7e22ee44079ea54523add43affecf
Gayan <gayanshaminda2001@gmail.com>
2025-01-06 12:08:06 +0530

    Home page lag fix

--- frontend/src/components/Goal.tsx
+++ frontend/src/components/Goal.tsx
@@ -1,14 +1,15 @@
 export default function Goal() {
   return (
     <section
-      className="relative w-full h-[80vh] bg-fixed bg-cover bg-no-repeat flex items-center justify-center"
-      style={{
-        backgroundImage: "url('/assets/tea-state.jpg')",
-        paddingTop: "50px",
-        paddingBottom: "100px",
-        backgroundPosition: "top",
-      }}
-    >
+  className="relative w-full h-[80vh] bg-cover bg-no-repeat flex items-center justify-center"
+  style={{
+    backgroundImage: "url('/assets/tea-state.jpg')",
+    paddingTop: "50px",
+    paddingBottom: "100px",
+    backgroundPosition: "top",
+  }}
+>
+
       <div className="text-center p-12 bg-black bg-opacity-50 mt-8">
         <h2 className="text-6xl font-extrabold text-white font-montserrat ">
           Our goal is to empower farmers with innovative tools and insights to
